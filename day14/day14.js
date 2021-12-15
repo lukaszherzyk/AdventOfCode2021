a6ac5d76566e75766c31837ed656e531ae9d5454
Łukasz Herzyk <lukasz.herzyk@gmail.com>
2021-12-15 08:57:50 +0100

    day 14 part 2

--- day14/day14.js
+++ day14/day14.js
@@ -23,52 +23,64 @@ input = require('fs')
 input = input.split('\n\n');
 const { performance } = require('perf_hooks');
 
+const addToMap = (m, s1, s2, v) => {
+    if (m.has(s1)) {
+        m.set(s1, { ...m.get(s1), ...(s2 ? { [s2]: (m.get(s1)[s2] || 0) + v } : {}) });
+    } else {
+        m.set(s1, {
+            ...(s2 ? { [s2]: v } : {})
+        });
+    }
+};
+const mapCounter = (m, s1, v) => {
+    if (m.has(s1)) {
+        m.set(s1, m.get(s1) + v);
+    } else {
+        m.set(s1, v);
+    }
+};
 const solution = (iterations) => {
     let template = input[0];
     const insertions = new Map();
-    const counter = new Map();
+    let c = new Map();
+    let m = new Map();
+
+    for (let j = 0; j < template.length; j++) {
+        const l = template[j];
+        const sib = template[j + 1];
+        addToMap(m, l, sib, 1);
+        mapCounter(c, l, 1);
+    }
 
     input[1].split('\n').forEach((e) => {
         const [a, b] = e.split(' -> ');
         insertions.set(a, b);
     });
-    template.split('').forEach((l) => {
-        if (counter.has(l)) {
-            counter.set(l, counter.get(l) + 1);
-        } else {
-            counter.set(l, 1);
+    for (let j = 0; j < iterations; j++) {
+        const newMap = new Map();
+        for (const [s1, value] of m) {
+            const valueKeys = Object.keys(value);
+            valueKeys.forEach((s2) => {
+                const insert = insertions.get(s1 + s2);
+                addToMap(newMap, insert, s2, value[s2]);
+                addToMap(newMap, s1, insert, value[s2]);
+                mapCounter(c, insert, value[s2]);
+            });
         }
-    });
-
-    for (let i = 0; i < iterations; i++) {
-        let newTemplate = '';
-        for (let j = 0; j < template.length - 1; j++) {
-            const pair = template.split('').slice(j, j + 2);
-            const x = insertions.get(pair.join(''));
-            newTemplate += (j === 0 ? pair[0] : '') + insertions.get(pair.join('')) + pair[1];
-        }
-        template = newTemplate;
-        console.log('template length', template.length);
-        console.log(`iter ${i + 1}:`, (performance.now() - now) / 1000);
-        //console.log('cache', cache);
-        console.log('=======');
+        m = new Map(newMap);
+    }
+    let arr = [];
+    for (const [key, value] of c) {
+        arr.push(value);
     }
 
-    template.split('').forEach((l) => {
-        if (counter.has(l)) {
-            counter.set(l, counter.get(l) + 1);
-        } else {
-            counter.set(l, 1);
-        }
-    });
-
-    const c = [];
-    for ([key, value] of counter) {
-        c.push(value);
-    }
-    return Math.max(...c) - Math.min(...c);
+    return Math.max(...arr) - Math.min(...arr);
 };
 
-const now = performance.now();
-console.log('part1:', solution(18));
-console.log('elapsed: ', (performance.now() - now) / 1000);
+let now = performance.now();
+console.log('part1:', solution(10));
+console.log('part1 time: ', performance.now() - now);
+
+now = performance.now();
+console.log('part2:', solution(40));
+console.log('part2 time: ', performance.now() - now);
